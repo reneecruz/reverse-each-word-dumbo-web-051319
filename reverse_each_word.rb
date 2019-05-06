39855d304682a13b8fe539fadfbea5ff2459275e
Renée Cruz <reneebasscruz@gmail.com>
2019-05-06 00:22:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence_string)
   reverse_sentence = sentence_string.split
-  reverse_sentence.each do |string| string.reverse
+  reverse_sentence.each do |string| string.reverse!
   end
 end
